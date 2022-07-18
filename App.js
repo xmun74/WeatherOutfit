b0105e3e65605e644a7d9569db1677a9fead3737
moon <xmun74@naver.com>
2022-07-18 16:54:09 +0900

    feat: expo location

--- App.js
+++ App.js
@@ -1,20 +1,122 @@
+import * as Location from "expo-location";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    // 권한 거절시
+    if (!granted) {
+      setOk(false);
+    }
+    // 현재 위도,경도 얻기
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    // 위도,경도로 도시명 얻기
+    const location = await Location.reverseGeocodeAsync(
+      {
+        latitude,
+        longitude,
+      },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>react native app !!!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="black" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <ScrollView
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
+
+      <ScrollView
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        contentContainerStyle={styles.weatherWeek}
+      >
+        <View>
+          <Text>지금</Text>
+          <Text>25도</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: "skyblue",
+  },
+  city: {
+    flex: 1,
     justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "beige",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  },
+  weather: {
+    backgroundColor: "pink",
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  temp: {
+    fontSize: 178,
+    fontWeight: "600",
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
+  },
+  weatherWeek: {
+    backgroundColor: "red",
   },
 });
